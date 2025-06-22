429fae9f592c7ae33c87f6e45ad5414a4a04f30b
Otávio Mendonça <otaviopedroso.mendonca@gmail.com>
2025-06-22 14:34:14 -0300

    Corrigindo o tipo de acesso dos atributos

--- Classes_Objetos/encapsulamento/PersonagemGame.java
+++ Classes_Objetos/encapsulamento/PersonagemGame.java
@@ -1,7 +1,7 @@
 public class PersonagemGame {
 
-    public int saudeAtual;
-    public String nome;
+    private int saudeAtual;
+    private String nome;
 
     public int getSaudeAtual() {
         return saudeAtual;
