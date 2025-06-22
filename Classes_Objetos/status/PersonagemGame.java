f90592876fec0796d5fc6101899d59121d4ba1f3
Otávio Mendonça <otaviopedroso.mendonca@gmail.com>
2025-06-22 16:28:31 -0300

    Task 3. Status do Personagem

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,52 @@
+public class PersonagemGame {
+
+    private int saudeAtual;
+    private String nome;
+    private String status;
+
+    public int getSaudeAtual() {
+        return saudeAtual;
+    }
+    public void setSaudeAtual(int saudeAtual) {
+        if (saudeAtual < 0) {
+            this.saudeAtual = 0;
+        }
+
+        else if (saudeAtual > 100) {
+            this.saudeAtual = 100;
+        }
+        
+        else {
+            this.saudeAtual = saudeAtual;
+        }
+        
+        if (saudeAtual > 0) {
+            status = "vivo";
+        }
+        else {
+            status ="morto";
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void tomarDano(int quantidadeDeDano) {
+        int novaSaude = this.saudeAtual - quantidadeDeDano;
+        setSaudeAtual(novaSaude);  
+    }
+
+    public void receberCura(int quantidadeDeCura) {
+        int novaSaude = this.saudeAtual + quantidadeDeCura;
+        setSaudeAtual(novaSaude);
+    }
+}
