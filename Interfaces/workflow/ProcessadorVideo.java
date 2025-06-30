66440c9907617a1dcd3ec1b0e47856f4dd0ac4be
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-06-30 16:27:43 +0000

    Task 0. Logging

---
+++ Interfaces/workflow/ProcessadorVideo.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProcessadorVideo {
+    private List<CanalNotificacao> canais;
+
+    public ProcessadorVideo() {
+        this.canais = new ArrayList<>();
+    }
+
+    public void registrarCanal(CanalNotificacao canal){
+        canais.add(canal);
+    }
+
+    public void processar(Video video) {
+        String texto = video.getArquivo() + " - " + video.getFormato();
+        Mensagem mensagem = new Mensagem(texto, TipoMensagem.LOG);
+
+        for (CanalNotificacao canal : canais) {
+            canal.notificar(mensagem);
+        }
+    }
+}
