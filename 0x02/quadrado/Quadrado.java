c3b3183e5a0f5d29792bae87d0d278d6615edb2c
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-07-03 16:39:57 +0000

    Corrigindo Excecao

--- 0x02/quadrado/Quadrado.java
+++ 0x02/quadrado/Quadrado.java
@@ -2,7 +2,7 @@ public class Quadrado {
 
     public static double area(double lado) {
         if (lado <= 0) {
-            throw new java.lang.IllegalArgumentException ("Lado deve possuir valor positivo");
+            throw new IllegalArgumentException ("Lado deve possuir valor positivo");
         } else {
             return lado * lado;
         }
