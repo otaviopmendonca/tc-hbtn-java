108c4b8bb70a9714838afb04f2a8a9949a2c1c18
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-07-07 14:54:24 +0000

    Cria diretorios base e adiciona arquivo inicial

---
+++ arquivos/0/FileWritingExercise.java
@@ -0,0 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class FileWritingExercise {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        // Solicita o nome do arquivo
+        System.out.print("Digite o nome do arquivo (com extensão .txt): ");
+        String fileName = scanner.nextLine();
+
+
+    // implemente o codigo aqui
+
+
+
+
+        scanner.close();
+    }
+}
