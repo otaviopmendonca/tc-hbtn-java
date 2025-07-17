b3cfcead4b5446b66b524cbee1d699a15035f3dd
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-07-17 16:40:00 +0000

    Hello World

---
+++ 0x00/java-hello_world/PrintStringConcat.java
@@ -0,0 +1,11 @@
+public class PrintStringConcat {
+    public static void main(String[] args) {
+        String texto1 = "Holberton";
+        String texto2 = "School";
+        String frase = texto1 + " " + texto2;
+        System.out.println("Bem vindo a " + frase + "!" );
+    }
+}
+
+
+
