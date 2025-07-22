16137c91ca7ce4a537a01639b59d4bb42324ef4a
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-07-22 20:04:27 +0000

    Task 2. Letra Maiúscula

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,11 @@
+public class Caractere {
+
+    public static boolean EhMaiusculo(char c) {
+        
+        if (c >= 'A' && c <= 'Z') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
