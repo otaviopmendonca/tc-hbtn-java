2a13e7e24c97d8a0f27e52c82442bbd76f48f23c
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-07-24 14:59:12 +0000

    Criado arquivos, Armazem.java em desenvolvimento

---
+++ Generics_Exercises/armazem/Armazem.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Armazem<T> implements Armazenavel<T> {
+    private Map<String, T> inventario;
+
+    public Armazem() {
+        inventario = new HashMap<>();
+    }
+
+    public void adicionarAoInventario()
+}
