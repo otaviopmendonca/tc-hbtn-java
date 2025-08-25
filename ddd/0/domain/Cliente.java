d59d0fc6e2ddc964503a8ed4307f9b5191be2825
otaviopmendonca <otaviopedroso.mendonca@gmail.com>
2025-08-25 18:01:49 +0000

    feat: adiciona sistema de gerenciamento de pedididos com DDD (em desenvolvimento)

---
+++ ddd/0/domain/Cliente.java
@@ -0,0 +1,31 @@
+package domain;
+
+
+public class Cliente {
+
+
+    private final String id;
+    private final String nome;
+
+
+    public Cliente(String id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
